e0cb1233ea94ec934fa83fd56aa8066294c80caa
Karimov Abdumalik <abdumal1k.ur@gmail.com>
2024-05-18 06:51:10 +0600

    I made the first level, created the Card component and displayed it in the App, and also correctly added the necessary files to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Card from "./components/Card/CardComponent.tsx";
 
 const App = () => (
-    <>
-
-    </>
-);
+    <div className="playingCards faceImages">
+        <Card rank="K" suit="diams" />
+        <Card rank="A" suit="hearts" />
+    </div>
+)
+;
 
 export default App
---
+++ src/components/Card/CardComponent.tsx
@@ -0,0 +1,28 @@
+// CardComponent.tsx
+import React from 'react';
+
+interface CardProps {
+    rank: string;
+    suit: string;
+}
+
+const CardComponent: React.FC<CardProps> = ({ rank, suit }) => {
+    const getSuitSymbol = (suit: string) => {
+        switch (suit) {
+            case 'diams': return '♦';
+            case 'hearts': return '♥';
+            case 'clubs': return '♣';
+            case 'spades': return '♠';
+            default: return '';
+        }
+    };
+
+    return (
+        <span className={`card rank-${rank} ${suit}`}>
+      <span className="rank">{rank}</span>
+      <span className="suit">{getSuitSymbol(suit)}</span>
+    </span>
+    );
+};
+
+export default CardComponent;
